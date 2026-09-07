219abacd3a58f477f74ef5baf0535e0ac66a336d
Macdonald Juma <macdonald.juma_oneac@MWC86E08309C79.DNS>
2026-09-07 20:48:19 +0200

    Fix module import syntax in server.js

--- db.js
+++ db.js
@@ -10,6 +10,12 @@ const pool = new Pool({
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
+  // External host requires SSL. rejectUnauthorized:false accepts self-signed certs,
+  // common on managed Postgres hosts. Set DB_SSL_REJECT_UNAUTHORIZED=true in env
+  // if your host has a trusted CA cert and you want strict verification instead.
+  ssl: process.env.DB_SSL === 'false'
+    ? false
+    : { rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED === 'true' },
 });
 
 pool.on('error', (err) => {
